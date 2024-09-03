9a83dd89d66770f5a0002a8ccdcb4d7d9c5321e5
hhyunn <cha9164@naver.com>
2024-09-03 18:52:09 +0900

    edit cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        // baseUrl: "http://localhost:3000",
+        baseUrl: "https://ci-cd-flame.vercel.app/",
+
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 });
